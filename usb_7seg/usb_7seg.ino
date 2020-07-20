fdbbfcee7167ce86e748be738124e4052ffb1229
uribo <ur1b0.dev@gmail.com>
2020-07-20 23:16:07 +0900

    とりあえず動く

--- usb_7seg/usb_7seg.ino
+++ usb_7seg/usb_7seg.ino
@@ -1,20 +1,28 @@
 //We always have to include the library
 #include "LedControl.h"
 
+typedef unsigned char uchar;
+
 /*
  Now we need a LedControl to work with.
  ***** These pin numbers will probably not work with your hardware *****
- pin 12 is connected to the DataIn 
- pin 11 is connected to the CLK 
- pin 10 is connected to LOAD 
+ pin 6 is connected to the DataIn 
+ pin 7 is connected to the CLK 
+ pin 8 is connected to LOAD 
  We have only a single MAX72XX.
  */
-LedControl lc=LedControl(7,9,8,1);
+LedControl lc=LedControl(6,7,8,1);
 
 /* we always wait a bit between updates of the display */
 unsigned long delaytime=250;
 
+#define STRING_MAX 4
+
 void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect
+  }
   /*
    The MAX72XX is in power-saving mode on startup,
    we have to do a wakeup call
@@ -27,51 +35,71 @@ void setup() {
 }
 
 
-/*
- This method will display the characters for the
- word "Arduino" one after the other on digit 0. 
- */
-void writeArduinoOn7Segment() {
-  lc.setChar(0,0,'a',false);
-  delay(delaytime);
-  lc.setRow(0,0,0x05);
-  delay(delaytime);
-  lc.setChar(0,0,'d',false);
-  delay(delaytime);
-  lc.setRow(0,0,0x1c);
-  delay(delaytime);
-  lc.setRow(0,0,B00010000);
-  delay(delaytime);
-  lc.setRow(0,0,0x15);
-  delay(delaytime);
-  lc.setRow(0,0,0x1D);
-  delay(delaytime);
-  lc.clearDisplay(0);
-  delay(delaytime);
-} 
+void loop() { 
+  static char sc_strings[ STRING_MAX + 1 ] = {0};
+  char tmp = 0;
+  uchar uc_loop_cnt = 0;
+  static uchar suc_char_cnt = 0;
+  static uchar suc_state = 0;
 
-/*
-  This method will scroll all the hexa-decimal
- numbers and letters on the display. You will need at least
- four 7-Segment digits. otherwise it won't really look that good.
- */
-void scrollDigits() {
-  for(int i=0;i<13;i++) {
-    lc.setDigit(0,3,i,false);
-    lc.setDigit(0,2,i+1,false);
-    lc.setDigit(0,1,i+2,false);
-    lc.setDigit(0,0,i+3,false);
-    lc.setDigit(0,7,i,false);
-    lc.setDigit(0,6,i+1,false);
-    lc.setDigit(0,5,i+2,false);
-    lc.setDigit(0,4,i+3,false);
-    delay(delaytime);
+  switch ( suc_state )
+  {
+  case 0: // 初期化
+    for( uc_loop_cnt = 0; uc_loop_cnt < STRING_MAX; uc_loop_cnt++ ){
+      sc_strings[uc_loop_cnt] = 0;
+    }
+    suc_char_cnt = 0;
+    suc_state++;
+    break;
+  case 1: // 受信する文字は数字のみ可
+    if( Serial.available() > 0 ){
+      tmp = Serial.read();
+      if( character_chack( tmp ) ){
+        sc_strings[ suc_char_cnt ] = tmp;
+        suc_char_cnt++;
+        suc_state++;
+      }
+      else{
+        lc.clearDisplay(0);
+        suc_state = 0;
+      }
+    }
+    break;
+  case 2: // 受信する文字は数字または改行コードのみ可
+    if( Serial.available() > 0 ){
+      tmp = Serial.read();
+      if( character_chack( tmp ) && ( suc_char_cnt < STRING_MAX ) ){
+        sc_strings[ suc_char_cnt ] = tmp;
+        suc_char_cnt++;
+      }
+      else if( '\n' == tmp ){
+        suc_state++;
+      }
+      else{
+        lc.clearDisplay(0);
+        suc_state = 0;
+      }
+    }
+    break;
+  case 3: // 表示
+    lc.clearDisplay(0);
+    for( uc_loop_cnt = 0; uc_loop_cnt < STRING_MAX; uc_loop_cnt++ ){
+      lc.setDigit(0, uc_loop_cnt + STRING_MAX - suc_char_cnt , sc_strings[ uc_loop_cnt ] - 0x30 ,false);
+    }
+    suc_state = 0;
+    break;
+  default:
+    break;
   }
-  lc.clearDisplay(0);
-  delay(delaytime);
+
 }
 
-void loop() { 
-  writeArduinoOn7Segment();
-  scrollDigits();
+char character_chack( char c ){
+  char ret = 0;
+
+  if( ( c >= '0' ) && ( c <= '9' ) ){
+    ret = 1;
+  }
+
+  return ret;
 }
